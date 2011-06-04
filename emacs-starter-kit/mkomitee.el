c10b28a899086d1f766e89967922a7503c4ead52
Michael Komitee <mkomitee@me.com>
2011-06-04 13:44:04 -0400

    emacs configs

--- emacs-starter-kit/mkomitee.el
+++ emacs-starter-kit/mkomitee.el
@@ -1,7 +1,19 @@
+;;; Make custom libraries available
 (add-to-list 'load-path (concat dotfiles-dir "/custom"))
+
+;;; Enable viper mode
+(setq viper-custom-file-name (concat dotfiles-dir "viper"))
 (setq viper-mode t)
 (require 'viper)
+
+;;; And vimpulse mode for good measure
 (require 'vimpulse)
 
+;;; Themes, ...
 (require 'color-theme-ir-black)
 (color-theme-ir-black)
+
+(setq tab-width 4)
+(setq c-basic-offset 4)
+(setq indent-tabs-mode t)
+	
