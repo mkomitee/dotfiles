e551246e023691abb94b08b25ae59c1b78f9f059
Michael Komitee <mkomitee@me.com>
2011-06-05 15:13:08 -0400

    viper mode

--- emacs-starter-kit/configs/cfg_generic.el
+++ emacs-starter-kit/configs/cfg_generic.el
@@ -16,3 +16,9 @@
 (setq default-tab-width 4)
 (setq tab-width 4)
 (setq c-basic-indent 4)
+
+;;; Only first learning emacs, use viper as a crutch
+(setq viper-mode t)
+(setq viper-custom-file-name "~/.emacs.d/viper")
+(require 'viper)
+(require 'vimpulse)
