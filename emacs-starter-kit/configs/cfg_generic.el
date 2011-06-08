fa85c009b47c214743e495ec067e21e2c8c07cfd
Michael Komitee <mkomitee@me.com>
2011-06-07 22:58:27 -0400

    mapped C-x C-; to super special comment mode (like nerd commenter in vim) disabled viper mode. Gotta learn sometime

--- emacs-starter-kit/configs/cfg_generic.el
+++ emacs-starter-kit/configs/cfg_generic.el
@@ -6,6 +6,23 @@
 (setq linum-format "%5.d ")
 (global-linum-mode 1)
 
+;; Original idea from
+;; http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+        If no region is selected and current line is not blank and we are not at
+        the end of the line, then comment current line.
+
+        Replaces default behaviour of comment-dwim, when it inserts comment at the
+        end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+
+(global-set-key (kbd "C-x C-;") 'comment-dwim-line)
+
 ;;; Setup spaces/tabs
 (setq-default indent-tabs-mode nil)
 (setq indent-tabs-mode nil)
@@ -15,11 +32,11 @@
 
 (setq speedbar-tag-split-minimum-length 100)
 ;;; Only first learning emacs, use viper as a crutch
-(setq viper-mode t)
-(setq viper-custom-file-name "~/.emacs.d/viper")
-(require 'viper)
-(require 'vimpulse)
-(vimpulse-vmap ",c" 'comment-dwim)
+;(setq viper-mode t)
+;(setq viper-custom-file-name "~/.emacs.d/viper")
+;(require 'viper)
+;(require 'vimpulse)
+;(vimpulse-vmap ",c" 'comment-dwim)
 
 (setq
   user-temporary-file-directory "~/.tmp/"
