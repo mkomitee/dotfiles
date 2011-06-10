f14604491f992f517f3c86208defdabad8f9345a
Michael Komitee <mkomitee@me.com>
2011-06-10 08:25:37 -0400

    Only remap C-x C-c when server is running Only start server if its not running Map % to be vi-like add zap-to-char to tips

--- emacs-starter-kit/configs/cfg_generic.el
+++ emacs-starter-kit/configs/cfg_generic.el
@@ -95,9 +95,22 @@
 
 ;;(server-start)
 
+(load "server")
+(unless (server-running-p) (server-start))
+
 (defun my-done ()
   (interactive)
   (server-edit)
   (make-frame-invisible nil t))
-(global-set-key (kbd "C-x C-c") 'my-done)
 
+(if (server-running-p)
+    (global-set-key (kbd "C-x C-c") 'my-done))
+
+(global-set-key "%" 'match-paren)
+
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
