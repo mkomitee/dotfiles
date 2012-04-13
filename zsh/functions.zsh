5af374f2ded71ef4a05aeaa589e0af106e160aab
Michael Komitee <komitee@deshaw.com>
2012-04-13 15:00:51 -0400

    cleaning up more stuff

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 function vim-clear-cache() {
-    for type in vimviews vimswap vimbackup ctrlp ; do
+    for type in vimviews vimswap vimbackup ctrlp neocomplcache ; do
         find ~/.vimdata/$type -type f -delete
     done
     find ~/.vimdata/ -name 'powerline.cache*' -type f -delete
+    find ~/.vimdata/ -name 'yankring*' -type f -delete
 }
 
 function psg() {
