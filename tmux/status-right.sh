c157690275f8972f49243a739194f1d2962e5b7f
Michael Komitee <komitee@deshaw.com>
2012-07-03 11:41:07 -0400

    pomodoro tmux integration

--- tmux/status-right.sh
+++ tmux/status-right.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 DATE=$(date "+%m/%d %I:%m %p")
-printf "#[fg=colour111,bold]$DATE"
+POM=$(pomodoro --short status)
+if [ $? = 0 ]; then
+    color=green
+else
+    color=red
+fi
+printf "#[fg=$color,bold]$POM #[fg=colour111,bold]$DATE"
