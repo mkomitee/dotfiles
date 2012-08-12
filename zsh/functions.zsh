f50a5eb96fb24d45db9dc712213a7aa79b366d3a
Michael Komitee <mkomitee@gmail.com>
2012-08-11 22:05:50 -0400

    simplifying vim-clear-cache

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,10 +1,6 @@
 #!/bin/zsh
 function vim-clear-cache() {
-    for type in vimviews vimswap vimbackup ctrlp neocomplcache ; do
-        find ~/.vimdata/$type -type f -delete
-    done
-    find ~/.vimdata/ -name 'powerline.cache*' -type f -delete
-    find ~/.vimdata/ -name 'yankring*' -type f -delete
+    find ~/.vimdata -type f -delete
 }
 
 function psg() {
