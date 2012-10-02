cde936e09ecee777ea450d18ed128df80d4b13b9
Michael Komitee <mkomitee@gmail.com>
2012-10-02 00:09:03 -0400

    Completion experiments

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,16 +1,16 @@
-unsetopt complete_in_word
-unsetopt correct
-unsetopt correct_all
-unsetopt glob_complete
-unsetopt menu_complete
 setopt always_to_end
 setopt auto_list
 setopt auto_menu
 setopt auto_param_slash
 setopt auto_remove_slash
+setopt complete_in_word
 setopt list_packed
 setopt list_types
 setopt rec_exact
+unsetopt correct
+unsetopt correct_all
+unsetopt glob_complete
+unsetopt menu_complete
 
 autoload -U compinit
 zmodload -i zsh/complist
@@ -19,6 +19,9 @@ compinit -i -u -d "${HOME}/.zcompdumps/${HOST%%.*}-${EUID}-$ZSH_VERSION"
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' verbose on
 zstyle ':completion:*' auto-description on
+
+# Case sensitive completion
+zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # Start menu completion if there are 2 ambiguous choices
 zstyle ':completion:*' menu select=2 
@@ -41,7 +44,28 @@ fi
 # Ignore certain files in completion
 zstyle ':completion:*:(all-|)files' ignored-patterns '*?.o' '*?~' '*?.pyc' '*?.pyo'
 
+[ -r /etc/ssh/ssh_known_hosts ] && _global_ssh_hosts=(${${${${(f)"$(</etc/ssh/ssh_known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
+[ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
+[ -r /etc/hosts ] && : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}} || _etc_hosts=()
 
-bindkey '^i' complete-word
+hosts=(
+  "$_global_ssh_hosts[@]"
+  "$_ssh_hosts[@]"
+  "$_etc_hosts[@]"
+  "$HOST"
+  localhost
+)
+zstyle ':completion:*:hosts' hosts $hosts
 
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path ~/.zcompcache/
 
+zstyle ':completion:*:*:*:users' ignored-patterns '_*'
+
+expand-or-complete-with-dots() {
+    echo -n "\e[31m......\e[0m"
+    zle expand-or-complete
+    zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
