90bba9d9b42695a704aeb7323b1b1121021ee2d0
Michael Komitee <mkomitee@gmail.com>
2013-02-22 22:06:58 -0500

    cleaning up unnecessary libs

--- emacs/lib/utils.el
+++ emacs/lib/utils.el
@@ -11,18 +11,4 @@
        (set-mark (point))
        (end-of-line)
        (call-interactively (function ,orig-function))))))
-
-;; emacs 24.1 doesn't have plist-to-alist, but color themes still use
-;; it, ... so we define it.
-(defun plist-to-alist (the-plist)
-  (defun get-tuple-from-plist (the-plist)
-    (when the-plist
-      (cons (car the-plist) (cadr the-plist))))
-
-  (let ((alist '()))
-    (while the-plist
-      (add-to-list 'alist (get-tuple-from-plist the-plist))
-      (setq the-plist (cddr the-plist)))
-      alist))
-
 (provide 'utils)
