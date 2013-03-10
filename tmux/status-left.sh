09641344016e5e7187a12262d21c350c8c067394
Michael Komitee <mkomitee@gmail.com>
2013-03-10 16:10:24 -0400

    tweaking tmux

--- tmux/status-left.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-POM=$(pomodoro.py status)
-if [ $? = 0 ]; then
-    color=green
-else
-    color=red
-fi
-printf "(#[fg=$color,bold]$POM#[default])"
--- tmux/status-right.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DATE=$(date "+%m/%d %I:%M %p")
-printf "#[fg=colour111,bold]$DATE#[default]"
