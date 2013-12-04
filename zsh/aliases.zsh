51a4fc6b0ec4ea6c54c87e9dbbc372d224c08fec
Michael Komitee <mkomitee@gmail.com>
2013-12-04 00:35:29 -0500

    Organize zsh/git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,26 +1,31 @@
+# Simple stats
 #!/bin/zsh
-alias tig='tig --all'
-alias bc='bc -l'
-alias s-s='sudo -s'
-alias tmux='tmux -u2'
-alias dot='cd ~dot'
-alias ack='noglob ack'
-alias turses='(source ~/.venv/turses/bin/activate && turses -a mkomitee)'
 alias -g bar_chart='python ~/.dotfiles/contrib/bitly/data_hacks.git/data_hacks/bar_chart.py'
 alias -g histogram='python ~/.dotfiles/contrib/bitly/data_hacks.git/data_hacks/histogram.py'
 alias -g ninety_five_percent='python ~/.dotfiles/contrib/bitly/data_hacks.git/data_hacks/ninety_five_percent.py'
 alias -g run_for='python ~/.dotfiles/contrib/bitly/data_hacks.git/data_hacks/run_for.py'
 alias -g sample='python ~/.dotfiles/contrib/bitly/data_hacks.git/data_hacks/sample.py'
-alias -g vi=vim
-alias lss=less
-alias boom='ruby ~/.dotfiles/contrib/holman/boom/bin/boom'
-alias curl='curl --negotiate --user :'
-alias irc='ssh -t matrix.gs tmux -u2 attach-session -t irc'
-alias aa='resume main'
-alias y=true
-alias reexec="exec zsh -l"
+
+# git aliases
+alias gaa="git add --all"
+alias gap="git add --patch"
+alias gcv="git commit -v"
 alias gd="git diff"
 alias gdc="git diff --cached"
 alias gds="git diff --staged"
-alias gap="git add --patch"
-alias gcv="git commit -v"
+
+alias -g vi=vim
+alias aa='resume main'
+alias ack='noglob ack'
+alias bc='bc -l'
+alias boom='ruby ~/.dotfiles/contrib/holman/boom/bin/boom'
+alias curl='curl --negotiate --user :'
+alias dot='cd ~dot'
+alias irc='ssh -t matrix.gs tmux -u2 attach-session -t irc'
+alias lss=less
+alias reexec="exec zsh -l"
+alias s-s='sudo -s'
+alias tig='tig --all'
+alias tmux='tmux -u2'
+alias turses='(source ~/.venv/turses/bin/activate && turses -a mkomitee)'
+alias y=true
