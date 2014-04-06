59a6086fc2b1a1e6be521e763f25062549ab74a3
Michael Komitee <mkomitee@gmail.com>
2014-04-06 14:05:42 -0400

    better lisp editing

--- emacs/lib/custom-lisp.el
+++ emacs/lib/custom-lisp.el
@@ -1,8 +1,12 @@
 (require-package 'rainbow-delimiters)
+(require-package 'paredit)
 
 (defun my-lisp-hook ()
   (progn
     (turn-on-eldoc-mode)
+    (require 'evil-paredit)
+    (paredit-mode t)
+    (evil-paredit-mode t)
     (rainbow-delimiters-mode t)))
 
 (add-hook 'emacs-lisp-mode-hook 'my-lisp-hook)
