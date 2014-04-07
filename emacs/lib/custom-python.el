ad8d76250cf7e4704b19d6ec6d8da6d15eab1228
Michael Komitee <mkomitee@gmail.com>
2014-04-06 21:52:37 -0400

    annotate pdb

--- emacs/lib/custom-python.el
+++ emacs/lib/custom-python.el
@@ -8,6 +8,16 @@
  python-shell-completion-setup-code ""
  python-shell-completion-string-code "';'.join(get_ipython().complete('''%s''')[1])\n")
 
+; Highlight the call to ipdb
+; src http://pedrokroger.com/2010/07/configuring-emacs-as-a-python-ide-2/
+(defun annotate-pdb ()
+  (interactive)
+  (highlight-lines-matching-regexp "import ipdb")
+  (highlight-lines-matching-regexp "ipdb.set_trace()")
+  (highlight-lines-matching-regexp "import pdb")
+  (highlight-lines-matching-regexp "pdb.set_trace()"))
+
+(add-hook 'python-mode-hook 'annotate-pdb)
 (add-hook 'python-mode-hook 'fci-mode)
 (add-hook 'python-mode-hook
           (lambda ()
