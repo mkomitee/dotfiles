26bd547ed8910fe8ca9259314fdf08a4d007141c
Michael Komitee <mkomitee@gmail.com>
2014-04-09 23:58:06 -0400

    fix yasnippet integration with evil

--- emacs/lib/custom-yasnippet.el
+++ emacs/lib/custom-yasnippet.el
@@ -1,12 +1,8 @@
-(defvar yas-fallback-behavior 'return-nil)
-(defvar yas-also-auto-indent-first-line t)
-
 (require-package 'yasnippet)
 (require 'yasnippet)
-(setq yas-snippet-dirs '("~/.dotfiles/emacs/snippets"))
 (yas-global-mode t)
 
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map [tab] nil)
+(evil-define-key 'insert yas-minor-mode-map (kbd "TAB")
+  'yas-next-field-or-maybe-expand)
 
 (provide 'custom-yasnippet)
