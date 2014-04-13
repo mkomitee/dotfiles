c65404a1352d57b655b84b17fe43c391f9eba70c
Michael Komitee <mkomitee@gmail.com>
2014-04-13 00:15:59 -0400

    limit that

--- emacs/lib/custom-autocomplete.el
+++ emacs/lib/custom-autocomplete.el
@@ -2,6 +2,7 @@
 (defvar ac-auto-start 1)
 (defvar ac-comphist-file (concat user-emacs-directory ".cache/ac-comphist.dat"))
 (defvar ac-quick-help-prefer-pos-tip nil)
+(defvar ac-max-width 0.5)
 (defvar ac-show-menu-immediately-on-auto-complete t)
 
 (require-package 'auto-complete)
