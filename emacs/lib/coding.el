7d3d9bbdb45529ee85c4e0bcd81aafb6f36d6881
Michael Komitee <mkomitee@gmail.com>
2014-04-20 23:37:44 -0400

    Setup haskell indentation

--- emacs/lib/coding.el
+++ emacs/lib/coding.el
@@ -99,5 +99,6 @@
 
 ;; haskell
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-to-list 'completion-ignored-extensions ".hi")
 (setq haskell-program-name "ghci")
