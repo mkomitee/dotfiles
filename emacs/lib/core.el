1b37abfe6aabe5394159da0692118e975d3b32fe
Michael Komitee <mkomitee@gmail.com>
2014-05-22 00:27:38 -0400

    disable these modes always

--- emacs/lib/core.el
+++ emacs/lib/core.el
@@ -1,6 +1,6 @@
-(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(unless (display-graphic-p) (menu-bar-mode -1))
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
 
 (setq inhibit-splash-screen t
       inhibit-startup-echo-area-message t
