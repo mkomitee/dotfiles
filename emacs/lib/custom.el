3891697f2535b4457eba05aa74909489e00892d9
Michael Komitee <komitee@deshaw.com>
2014-05-30 18:16:21 -0400

    writegood

--- emacs/lib/custom.el
+++ emacs/lib/custom.el
@@ -10,9 +10,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(default ((t (:background "#1B1D1E" :foreground "#F8F8F2"))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "snow"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "light slate blue"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "honeydew"))))
  '(rainbow-delimiters-depth-4-face ((t (:foreground "royal blue"))))
- '(rainbow-delimiters-unmatched-face ((t (:foreground "red")))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))
+ '(writegood-passive-voice-face ((t (:inherit font-lock-warning-face :background "rosy brown")))))
