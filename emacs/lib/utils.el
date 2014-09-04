f0b6ce34f490c14e610f7e3f4ba84c9835c42bc4
Michael Komitee <komitee@deshaw.com>
2014-09-04 17:44:19 -0400

    escape damnit

--- emacs/lib/utils.el
+++ emacs/lib/utils.el
@@ -70,3 +70,23 @@ vi style of % jumping to matching brace."
     (byte-recompile-directory (concat komitee/emacs-config-directory "/lib") 0)
     (byte-compile-file (concat komitee/emacs-config-directory "/init.el"))
     (byte-compile-file komitee/local-emacs-config)))
+
+;; esc quits
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+(global-set-key [escape] 'evil-exit-emacs-state)
