c5ceab5c8bae355b7ac2c2876e61aeb6091838bc
Michael Komitee <mkomitee@gmail.com>
2015-01-11 17:29:04 -0500

    Better maps for evil-args

--- emacs/maps.el
+++ emacs/maps.el
@@ -108,18 +108,11 @@
 
 (req-package evil-args
   :config (progn
-            ;; bind evil-args text objects
             (define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
             (define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
-
-            ;; bind evil-forward/backward-args
-            (define-key evil-normal-state-map "L" 'evil-forward-arg)
-            (define-key evil-normal-state-map "H" 'evil-backward-arg)
-            (define-key evil-motion-state-map "L" 'evil-forward-arg)
-            (define-key evil-motion-state-map "H" 'evil-backward-arg)
-
-            ;; bind evil-jump-out-args
-            (define-key evil-normal-state-map "K" 'evil-jump-out-args)
+            (define-key evil-motion-state-map (kbd "M-l") 'evil-forward-arg)
+            (define-key evil-motion-state-map (kbd "M-h") 'evil-backward-arg)
+            (define-key evil-motion-state-map (kbd "M-k") 'evil-jump-out-args)
             )
   )
 
