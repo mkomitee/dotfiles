81396edb2c86fcfd8c67acc7085b6e1517cc22ad
Michael Komitee <komitee@deshaw.com>
2015-02-04 15:16:04 -0500

    Black as blackest black

--- emacs/custom.el
+++ emacs/custom.el
@@ -13,6 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background "#000000"))))
  '(evil-visual-mark-face
    ((t (:background "color-89" :foreground "yellow" :underline t))))
  '(flyspell-incorrect
