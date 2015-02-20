0e717ad7e03059efbe814a5b469096fb74ea196e
Michael Komitee <komitee@deshaw.com>
2015-02-20 15:06:05 -0500

    Disable the mouse

--- emacs/core.el
+++ emacs/core.el
@@ -234,3 +234,10 @@
 (req-package yaml-mode
   :config (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
   )
+
+(dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
+             [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+             [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+             [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+             [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+  (global-unset-key k))
--- emacs/evil.el
+++ emacs/evil.el
@@ -102,6 +102,11 @@
             (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
             (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
             (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+            (define-key evil-motion-state-map [down-mouse-1] nil)
+            (define-key evil-visual-state-map [mouse-2] nil)
+            (define-key evil-normal-state-map [mouse-2] nil)
+            (define-key evil-insert-state-map [mouse-2] nil)
             )
   )
 
