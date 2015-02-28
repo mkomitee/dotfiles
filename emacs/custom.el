52fc75e6f42b90deaa00a4044bec5d59479ef3d5
Michael Komitee <mkomitee@gmail.com>
2015-02-28 17:09:52 -0500

    Customize ace-window face

--- emacs/custom.el
+++ emacs/custom.el
@@ -13,6 +13,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face
+   ((t (:inherit ace-jump-face-foreground :height 3.0))))
  '(default ((t (:background "#000000"))))
  '(evil-search-highlight-persist-highlight-face
    ((t (:inherit lazy-highlight))))
