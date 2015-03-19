13acba109308e6643a056ce2561a407a2527f334
Michael Komitee <komitee@deshaw.com>
2015-03-19 15:09:07 -0400

    trust more themes

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote ("2a12e95e9ee6ed57592e7df12f3f028205575e9b3affdb5e6fa589421c618136"
+   (quote ("6cb86285d0cee020bcb0e9d0ffa56bad4c5be86d2bd14f2f4997a46130a6fe56"
+           "7e8d09164066c3fc1f27a46cb9797e890648a52077fcfe07d91f041d9864a56d"
+           "2a12e95e9ee6ed57592e7df12f3f028205575e9b3affdb5e6fa589421c618136"
            "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e"
            "0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832"
            default))))
@@ -14,7 +16,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(default ((t (:background "#000000"))))
  '(aw-leading-char-face
    ((t (:inherit ace-jump-face-foreground :height 3.0))))
  '(evil-search-highlight-persist-highlight-face
