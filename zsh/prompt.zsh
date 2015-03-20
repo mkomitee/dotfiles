76d55adcb7340c479060d38ff43f7838d2e268fe
Michael Komitee <komitee@deshaw.com>
2015-03-20 17:12:58 -0400

    remove git, add timer to prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,42 +1,15 @@
 #!/usr/bin/env zsh
 
+# Disable default virtualenv prompt info
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
 autoload colors; colors;
 setopt prompt_subst
 setopt transient_rprompt
+MKPROMPT_TIMER=0
+MKPROMPT_CUTOFF=10
 
-function git_prompt() {
-    local ref
-    ref=$(git symbolic-ref HEAD 2> /dev/null)
-    ref="${ref#refs/heads/}"
-    if [ "$ref" != "" ]; then
-        local clean
-        local cached
-        local ahead
-        local behind
-        local echoed
-
-        clean=$(git diff --shortstat 2> /dev/null)
-        cached=$(git diff --cached --shortstat 2> /dev/null)
-        remote=${$(git rev-parse --verify ${hook_com[branch]}@{upstream} --symbolic-full-name 2>/dev/null)/refs\/remotes\/}
-        if [ -n "$remote" ]; then
-            ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
-            behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
-        fi
-        if [ "$clean$cached$ahead$behind" != "00" ]; then
-            echo -n "%{$fg_bold[red]%}+%{$reset_color%}"
-            echoed=1
-        fi
-        if [ "$ref" != "master" ]; then
-            echo -n "%{$fg[yellow]%}($ref)%{$reset_color%}"
-            echoed=1
-        fi
-        if [ -n "$echoed" ]; then
-            echo -n ' '
-        fi
-    fi
-}
-
-function shorthost() {
+function mkprompt_host() {
     if [ "$KEYMAP" = 'vicmd' ]; then
         echo "%{$fg[blue]%}%2m%{$reset_color%}"
     else
@@ -45,7 +18,7 @@ function shorthost() {
 
 }
 
-function prompt() {
+function mkprompt_prompt() {
     if [ "$USER" = 'root' ]; then
         echo "%{$fg[red]%}%#%{$reset_color%}"
     else
@@ -53,80 +26,50 @@ function prompt() {
     fi
 }
 
-
-function jobs_prompt() {
+function mkprompt_jobs() {
     local JOBS=$(jobs | wc -l | sed 's/ //g')
     if [ "$JOBS" != "0" ]; then
         echo "%{$fg[yellow]%}($JOBS)%{$reset_color%} "
     fi
 }
 
-# Disable default virtualenv prompt info so we can do our own
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-function virtualenv_prompt() {
-    if [ "$VIRTUAL_ENV" != "" ]; then
-        VENV=$(basename $VIRTUAL_ENV)
-        if [ "$VENV" != "$(uname).$(arch)" ]; then
-            echo "[$VENV] "
-        fi
-    fi
-}
-
-function exit_code() {
+function mkprompt_exit() {
     echo "%(?,,%{$fg[red]%}[%?]%{$reset_color%} )"
 }
 
-function history_prompt() {
-    echo "%{$fg[yellow]%}!%!%{$reset_color%}"
-}
-
-function enable_prompt() {
-    unset PROMPT_DISABLED
-}
-
-function current_dir() {
+function mkprompt_cwd() {
     echo "%{$fg[yellow]%}%3~%{$reset_color%}"
 }
 
-function setup_prompt() {
-    if [ "$PROMPT_DISABLED" != "1" ]; then
-        PROMPT='$(exit_code)$(shorthost) $(virtualenv_prompt)$(git_prompt)$(current_dir) $(jobs_prompt)$(prompt) '
-    else
-        PROMPT='$(exit_code)$(shorthost) $(virtualenv_prompt)$(current_dir) $(jobs_prompt)$(prompt) '
+function mkprompt_timer() {
+    MKPROMPT_TIMER=$(date +%s)
+}
+
+function mkprompt_elapsed() {
+    if [[ ${MKPROMPT_TIMER} -ne 0 ]]; then
+        local elapsed=$(($(date +%s) - $MKPROMPT_TIMER))
+        if [[ $elapsed -ge  ${MKPROMPT_CUTOFF} ]]; then
+            local hours=$((${elapsed}/3600))
+            local remainder=$((${elapsed}%3600))
+            local minutes=$((${remainder}/60))
+            local seconds=$((${remainder}%60))
+            local clock=$(printf '%d:%02d:%02d' $hours $minutes $seconds)
+            echo "%{$fg[red]%}[${clock}]%{$reset_color%} "
+        fi
     fi
+    MKPROMPT_TIMER=0
 }
 
-function disable_prompt() {
-    PROMPT_DISABLED=1
-}
-
-function toggle_prompt() {
-    if [ -z "$PROMPT_DISABLED" ]; then
-        disable_prompt
-    else
-        enable_prompt
-    fi
-}
-
-function title {
-  [ "$TERM" != "linux" ] || return
-  [ "$DISABLE_AUTO_TITLE" != "true" ] || return
-  printf '\033]2;%s\033\\' "$1"
-}
-
-# Set the window title (tmux pane) to indicate we're ready for a command, also
-# setup the prompt.
-function my_precmd {
-  setup_prompt
-  title "shell"
-}
-
-# Set the window title (tmux pane) to the command we're running. This is for vim
-# & window pane navigation integration
-function my_preexec {
-  title "$1"
+function mkprompt_setup() {
+    PROMPT=""
+    PROMPT="${PROMPT}$(mkprompt_exit)"
+    PROMPT="${PROMPT}$(mkprompt_elapsed)"
+    PROMPT="${PROMPT}$(mkprompt_host) "
+    PROMPT="${PROMPT}$(mkprompt_cwd) "
+    PROMPT="${PROMPT}$(mkprompt_jobs)"
+    PROMPT="${PROMPT}$(mkprompt_prompt) "
 }
 
 autoload -U add-zsh-hook
-add-zsh-hook precmd  my_precmd
-add-zsh-hook preexec my_preexec
+add-zsh-hook precmd mkprompt_setup
+add-zsh-hook preexec mkprompt_timer
