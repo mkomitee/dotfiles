1796ebed97d1b7bfbb09c95f9c7f36fd036604f7
Michael Komitee <komitee@deshaw.com>
2015-03-24 11:14:33 -0400

    tweak git-gutter

--- emacs/vcs.el
+++ emacs/vcs.el
@@ -1,8 +1,3 @@
-(req-package git-gutter
-  :diminish git-gutter-mode
-  :config (global-git-gutter-mode)
-  )
-
 (req-package gitconfig-mode
   :config (add-to-list 'auto-mode-alist '("gitconfig\\'" . gitconfig-mode))
   )
@@ -18,6 +13,43 @@
               "]h" 'git-gutter:next-hunk
               "[h" 'git-gutter:previous-hunk
               )
+            ;; Please adjust fringe width if your own sign is too big.
+            (setq-default left-fringe-width 20
+                          right-fringe-width 20)
+
+            (fringe-helper-define 'git-gutter-fr:added nil
+              ".XXXXXX."
+              "XX....XX"
+              "X......X"
+              "X......X"
+              "XXXXXXXX"
+              "XXXXXXXX"
+              "X......X"
+              "X......X")
+
+            (fringe-helper-define 'git-gutter-fr:deleted nil
+              "XXXXXX.."
+              "XX....X."
+              "XX.....X"
+              "XX.....X"
+              "XX.....X"
+              "XX.....X"
+              "XX....X."
+              "XXXXXX..")
+
+            (fringe-helper-define 'git-gutter-fr:modified nil
+              "XXXXXXXX"
+              "X..XX..X"
+              "X..XX..X"
+              "X..XX..X"
+              "X..XX..X"
+              "X..XX..X"
+              "X..XX..X"
+              "X..XX..X")
+
+            (setq git-gutter:update-interval 1
+                  git-gutter:hide-gutter 1)
+            (global-git-gutter-mode)
             )
   )
 
