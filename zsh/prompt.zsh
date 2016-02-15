c289df543852c86f66488361eae8ba43225361f3
Michael Komitee <mkomitee@gmail.com>
2016-02-15 15:58:23 -0500

    Tweak prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,36 +1,69 @@
-#!/usr/bin/env zsh
+# #!/usr/bin/env zsh
 
-# Disable default virtualenv prompt info
-export VIRTUAL_ENV_DISABLE_PROMPT=1
+MKPROMPT_CUTOFF=3
+MKPROMPT_KEYMAP=
 
-autoload colors; colors;
+autoload -Uz promptinit
+promptinit
+autoload colors;
+colors;
+autoload -Uz add-zsh-hook
+
 setopt prompt_subst
 setopt transient_rprompt
-MKPROMPT_TIMER=0
-MKPROMPT_CUTOFF=10
+
+# %! %h          - Current history event number
+# %#             - # if superuser, else %
+# %%             - a literal %
+# %)             - use with %X(.tstr.fstr)
+# %*             - Time in 24-hour format with seconds
+# %/ %d          - $PWD
+# %c %. %C       - Deprecated alternatives, differ by default n
+# %?             - Return status of last command
+# %@ %t          - Time of day in am/pm format
+# %B (%b)        - Start (stop) bold face mode
+# %D %D{str}     - Date as YY-MM-DD, optional strftime spec
+# %E             - Clear to end of line
+# %i             - Script/function line number ($LINENO)
+# %j             - Number of jobs listed by jobs
+# %L             - Shell depth ($SHLVL)
+# %l             - Login terminal without /dev or /dev/tty
+# %M             - Full host name
+# %m             - Host name to first dot or n dots
+# %N             - Name of script, function, sourced file
+# %n             - Name of user ($USERNAME)
+# %S %s          - Start (stop) standout mode
+# %T             - Time of dsay, 24-hour format
+# %U %u          - Start (stop) underline mode
+# %v             - nth component of $psvar array
+# %W             - Date as middle-endian MM/DD/YY
+# %w             - Date as DAY DD
+# %y             - Login terminal without /dev
+# %_             - Parser state (continuation lines, debug)
+# %~             - Like %/, %d but with tilde substitution
+# %{esc%}        - Escape sequence (esc doesn't move cursor)
+# %X(.tstr.fstr) - tstr if test X gives n, else fstr
+# %<str<         - Truncate to n on left, str on left if so
+# %>str>         - Truncate to n on right, str on right if so
+
+function mkprompt_history() {
+    echo "%(!,%{$fg[red]%},%{$fg[yellow]%})=%!= %{$reset_color%}"
+}
 
 function mkprompt_host() {
-    if [ "$KEYMAP" = 'vicmd' ]; then
-        echo "%{$fg[blue]%}%2m%{$reset_color%}"
+    if [ "$MKPROMPT_KEYMAP" = 'vicmd' ]; then
+        echo "%{$fg[blue]%}%m%{$reset_color%}"
     else
-        echo "%(!.%{$fg[red]%}.%{$fg[yellow]%})%2m%{$reset_color%}"
+        echo "%(!.%{$fg[red]%}.%{$fg[yellow]%})%m%{$reset_color%}"
     fi
-
 }
 
 function mkprompt_prompt() {
-    if [ "$USER" = 'root' ]; then
-        echo "%{$fg[red]%}%#%{$reset_color%}"
-    else
-        echo "%{$fg[yellow]%}%#%{$reset_color%}"
-    fi
+    echo "%(!,%{$fg[red]%},%{$fg[yellow]%})%#%{$reset_color%}"
 }
 
 function mkprompt_jobs() {
-    local JOBS=$(jobs | wc -l | sed 's/ //g')
-    if [ "$JOBS" != "0" ]; then
-        echo "%{$fg[yellow]%}($JOBS)%{$reset_color%} "
-    fi
+    echo "%(1j,%{$fg[yellow]%}(%j%)%{$reset_color%} ,)"
 }
 
 function mkprompt_exit() {
@@ -38,23 +71,23 @@ function mkprompt_exit() {
 }
 
 function mkprompt_cwd() {
-    echo "%{$fg[yellow]%}%3~%{$reset_color%}"
+    echo "%(!,%{$fg[red]%},%{$fg[yellow]%})%3~%{$reset_color%}"
 }
 
 function mkprompt_timer() {
-    MKPROMPT_TIMER=$(date +%s)
+    MKPROMPT_TIMER=${SECONDS}
 }
 
 function mkprompt_elapsed() {
     if [[ ${MKPROMPT_TIMER} -ne 0 ]]; then
-        local elapsed=$(($(date +%s) - $MKPROMPT_TIMER))
+        local elapsed=$(($SECONDS - $MKPROMPT_TIMER))
         if [[ $elapsed -ge  ${MKPROMPT_CUTOFF} ]]; then
             local hours=$((${elapsed}/3600))
             local remainder=$((${elapsed}%3600))
             local minutes=$((${remainder}/60))
             local seconds=$((${remainder}%60))
             local clock=$(printf '%d:%02d:%02d' $hours $minutes $seconds)
-            echo "%{$fg[red]%}[${clock}]%{$reset_color%} "
+            echo "%{$fg[red]%}[${clock}]%{$reset_color%}"
         fi
     fi
     MKPROMPT_TIMER=0
@@ -63,13 +96,20 @@ function mkprompt_elapsed() {
 function mkprompt_setup() {
     PROMPT=""
     PROMPT="${PROMPT}$(mkprompt_exit)"
-    PROMPT="${PROMPT}$(mkprompt_elapsed)"
+    PROMPT="${PROMPT}$(mkprompt_history)"
     PROMPT="${PROMPT}$(mkprompt_host) "
     PROMPT="${PROMPT}$(mkprompt_cwd) "
     PROMPT="${PROMPT}$(mkprompt_jobs)"
     PROMPT="${PROMPT}$(mkprompt_prompt) "
+    RPROMPT=""
+    RPROMPT="${RPROMPT}$(mkprompt_elapsed)"
 }
 
-autoload -U add-zsh-hook
+function zle-line-init zle-keymap-select {
+    MKPROMPT_KEYMAP=${KEYMAP}
+    mkprompt_setup
+    zle reset-prompt
+}
+
 add-zsh-hook precmd mkprompt_setup
 add-zsh-hook preexec mkprompt_timer
