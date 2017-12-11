0eaab632e2f9fa8ecb06973f5441cf44c613da17
Michael Komitee <komitee@deshaw.com>
2017-12-11 11:10:02 -0500

    Cleanup tmux

---
+++ tmux/tmux-clipboard.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Integrate copy/paste w/ the x & macos clipboards.
+tmux unbind-key -T copy-mode-vi 'y'
+tmux unbind p
+if [ "$(uname)" = 'Linux' ]; then
+    tmux bind-key -T copy-mode-vi 'y' send -X copy-pipe "xclip -i"
+    tmux bind-key p run-shell 'tmux set-buffer "$(xclip -o)"; tmux paste-buffer'
+else
+    tmux set-option -g default-command "reattach-to-user-namespace -l zsh"
+    tmux bind-key -T copy-mode-vi 'y' send -X copy-pipe "pbcopy"
+    tmux bind-key p run-shell 'tmux set-buffer "$(pbpaste)"; tmux paste-buffer'
+fi
