60c3516d15606c31a1e1c2c25f770186786e3a04
Michael Komitee <komitee@deshaw.com>
2018-04-02 07:25:13 -0400

    ohyeah

---
+++ vim/after/plugin/ale.vim
@@ -0,0 +1,9 @@
+
+" hi ALEError   cterm=none gui=undercurl guisp=#fb4934
+" hi ALEWarning cterm=none gui=undercurl guisp=#fabd2f
+" hi ALEInfo    cterm=none gui=undercurl guisp=#83a598
+hi! link ALEError GruvboxRed
+hi! link ALEWarning GruvboxYellow
+hi! link ALEInfo GruvboxBlue
+
+
---
+++ vim/templates/skeleton.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""Make it so."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import with_statement
+
+
+def main():
+    """Make it so."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
