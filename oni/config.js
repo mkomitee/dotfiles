4327892deb01f15a669b6a7eaaeb3137a34ed49b
Michael Komitee <mkomitee@gmail.com>
2018-06-19 10:25:28 -0400

    nvim/oni

---
+++ oni/config.js
@@ -0,0 +1,30 @@
+"use strict";
+exports.__esModule = true;
+exports.activate = function (oni) {
+    console.log("config activated");
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", function () { return console.log("Control+Enter was pressed"); });
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+};
+exports.deactivate = function (oni) {
+    console.log("config deactivated");
+};
+exports.configuration = {
+    //add custom config here, such as
+    "ui.colorscheme": "gruvbox",
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    //"oni.loadInitVim": false,
+    //"editor.fontSize": "12px",
+    //"editor.fontFamily": "Monaco",
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+    "sidebar.default.open": false
+};
